caafa96bd5c3d46fb77e4fa8ce16adb0fd76c172
josep <josepmg05@gmail.com>
2024-02-26 09:41:25 +0100

    New rentals

--- REST_API/carrental/server.js
+++ REST_API/carrental/server.js
@@ -1,11 +1,43 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const fs = require('fs')
 const port = 8080
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, './index.html'));
 })
 
 app.listen(port, () => {
   console.log(`PTI HTTP Server listening at http://localhost:${port}`)
+})
+
+app.post('/newrental', (req, res, next) => {
+
+  /* Check if file exists and create it if not */
+  if(!fs.existsSync('rentals.json')){
+    rentalsJSON = {"rentals": []};
+    fs.writeFileSync("rentals.json", JSON.stringify(rentalsJSON));
+  }
+
+  /* Read file */
+  rentalsFileRawData = fs.readFileSync('rentals.json');
+  rentalsJSON = JSON.parse(rentalsFileRawData);
+
+  /* Create new rental JSON and add it into the parsed array */
+  new_rental = {
+    "maker":req.body.maker,
+    "model":req.body.model,
+    "days":req.body.days,
+    "units":req.body.units
+  }
+  rentalsJSON['rentals'].push(new_rental)
+
+  /* Write array into the file */
+  fs.writeFileSync("rentals.json", JSON.stringify(rentalsJSON));
+
+  console.log(new_rental)
+  res.end();
 })
